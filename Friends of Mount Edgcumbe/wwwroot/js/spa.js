2273106b9dbe906d8fc4f11aabb9a4eb9e0e79a5
leerpgbai <leerpgbai@hotmail.com>
2021-01-20 17:40:45 +0000

    Completed SPA 03 exercise. Pushing to save before moving onto SPA04

---
+++ Friends of Mount Edgcumbe/wwwroot/js/spa.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#map').click(function () {
+        $('#page-content-wrapper').load('map.html');
+    })
+});$(document).ready(function () {
+    $('#treasure').click(function () {
+        $('#page-content-wrapper').load('treasure.html');
+    })
+});$(document).ready(function () {
+    $('#data').click(function () {
+        $('#page-content-wrapper').load('data.html');
+    })
+});
